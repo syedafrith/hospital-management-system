68e8b02b69094e4d9301876798cf2f24bbf17a55
syedafrith <syedafrithsta03@gmail.com>
2020-09-03 16:10:53 +0530

    firstcommit

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -73,13 +73,9 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Django',
-        'USER': 'root',
-        'PASSWORD': 'afrithsta@0903',
-        'HOST': 'localhost',
-        'PORT': '3306',
+ 'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
